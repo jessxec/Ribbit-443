9647481f9d403216cde2fb4316394295edc8e943
jessxec <jessiec1426@gmail.com>
2024-10-28 17:18:27 -0400

    starter swift app

---
+++ Ribbit/RibbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RibbitApp.swift
+//  Ribbit
+//
+//  Created by Jessie Chen on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct RibbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
